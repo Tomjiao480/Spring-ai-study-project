f5f7c1defd2b8a00fe885a3526ccbc3b14eccd6a
Jiaofeiyang <15210446931@163.com>
2025-06-13 18:10:56 +0800

    spring-AI-流式对话Stream

--- spring-ai-chat-demo/src/main/java/com/ningning0111/controller/DemoController.java
+++ spring-ai-chat-demo/src/main/java/com/ningning0111/controller/DemoController.java
@@ -2,15 +2,23 @@ package com.ningning0111.controller;
 
 import org.springframework.ai.chat.ChatClient;
 import org.springframework.ai.chat.StreamingChatClient;
+import org.springframework.ai.chat.messages.Message;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class DemoController {
     private final ChatClient  chatClient;
     private final StreamingChatClient streamingChatClient;
+
+    //历史消息列表
+    static List<Message> historyMessage = new ArrayList<>();
+
     public DemoController(ChatClient chatClient, StreamingChatClient streamingChatClient) {
         this.chatClient = chatClient;
         this.streamingChatClient = streamingChatClient;
@@ -35,5 +43,6 @@ public class DemoController {
 
         return streamingChatClient.stream(prompt).flatMapSequential(Flux::just);
     }
+
 
 }
